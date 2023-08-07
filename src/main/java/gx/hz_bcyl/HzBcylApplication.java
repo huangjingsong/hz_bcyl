f9fe4f97f9bfc3388a2b278cccd4f598bc60f274
黄敬淞 <huangjingsong1230@163.com>
2023-08-08 00:46:20 +0800

    Initial commit

---
+++ src/main/java/gx/hz_bcyl/HzBcylApplication.java
@@ -0,0 +1,13 @@
+package gx.hz_bcyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HzBcylApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HzBcylApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gx/hz_bcyl/HzBcylApplicationTests.java
@@ -0,0 +1,13 @@
+package gx.hz_bcyl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HzBcylApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
